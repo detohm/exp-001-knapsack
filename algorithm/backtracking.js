2bf6b794b015711f89e64bffde1442aa92b115a0
Ohm Attaphong <ohm.attaphong@gmail.com>
2017-06-01 00:48:16 +0700

    Implement Backtracking approach

---
+++ algorithm/backtracking.js
@@ -0,0 +1,63 @@
+
+let finalResult = {
+  weight: 0,
+  optimalValue: 0,
+  solution: []
+}
+
+const bruteforce = (data) => {
+
+  dfs([], 0, data.weights, data.values, data.limitWeight)
+
+  return finalResult
+}
+
+const validate = (tempArr,weights,limitWeight) => {
+
+  let tempWeight = 0
+
+  for(let i = 0; i < tempArr.length ; i++){
+    if(tempArr[i] === 1){
+      tempWeight += weights[i]
+    }
+  }
+
+  if(tempWeight > limitWeight) return false
+  return true
+}
+
+
+const dfs = (tempArr, index, weights, values, limitWeight) => {
+
+  if(!validate(tempArr,weights,limitWeight)){ return }
+
+  //reach the end of state-space search tree
+  if(index == weights.length){
+
+    let sumWeight = 0
+    let sumValue = 0
+
+    for(let i = 0; i < tempArr.length; i++){
+      if(tempArr[i] === 1){
+        sumWeight += weights[i]
+        sumValue += values[i]
+      }
+    }
+
+    //update optimal solution if found
+    if(sumWeight <= limitWeight && sumValue > finalResult.optimalValue ){
+      finalResult.weight = sumWeight
+      finalResult.optimalValue = sumValue
+      finalResult.solution = tempArr
+    }
+
+    return
+
+  }
+
+  dfs(tempArr.concat(0),index+1, weights, values, limitWeight)
+  dfs(tempArr.concat(1),index+1, weights, values, limitWeight)
+}
+
+
+module.exports = bruteforce
--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const backtracking = require('./algorithm/backtracking')
 var finalResult = []
 var finalValue = 0
 
-var data = dataLoader.load('./dataset/s-1.txt')
+var data = dataLoader.load('./dataset/s-3.txt')
 let startTime, endTime = new Date()
+
 
 //Bruteforce using DFS
 console.log(`Starting Depth-First Search...`)
@@ -17,3 +18,15 @@ var result = bruteforce(data)
 endTime = new Date() - startTime
 console.log({result})
 console.log(`Depth-First Search : Solved in ${endTime} ms\n`)
+
+
+//Backtracking
+console.log(`Starting Backtracking...`)
+startTime = new Date()
+
+var result = backtracking(data)
+
+endTime = new Date() - startTime
+console.log({result})
+console.log(`Depth-First Search : Solved in ${endTime} ms\n`)
+
